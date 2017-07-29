48de2132e8ec4c388a0964af277a8380e6905c1c
kelvin mbwilo <kelvinmbwilo@gmail.com>
2017-07-29 11:31:20 +0300

    adding pagination module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TreeModule } from 'angular-tree-component';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { DndModule } from 'ng2-dnd';
+import {ClickOutsideDirective} from './directives/click-outside.directive';
+import {FilterByNamePipe} from './pipes/filter-by-name.pipe';
+import {SafeHtmlPipe} from './pipes/safe-html.pipe';
 
 @NgModule({
   imports: [
@@ -11,6 +14,13 @@ import { DndModule } from 'ng2-dnd';
     ColorPickerModule,
     DndModule.forRoot()
   ],
-  declarations: []
+  declarations: [
+    ClickOutsideDirective,
+    FilterByNamePipe,
+    SafeHtmlPipe
+  ],
+  exports: [
+
+  ]
 })
 export class SharedModule { }
