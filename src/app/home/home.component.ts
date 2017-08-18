89fc091280e2e6d8f0f478bb83f27679cf580a82
kelvin mbwilo <kelvinmbwilo@gmail.com>
2017-08-18 07:24:31 +0300

    removing duplicate items in card indicator and fixing fake scorecard in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import {
-  Component, OnInit,  style, state, animate, transition, trigger
+  Component, OnInit, style, state, animate, transition, trigger
 } from '@angular/core';
-import {ScoreCard,ScorecardService} from "../shared/services/scorecard.service";
-import {OrgUnitService} from "../shared/services/org-unit.service";
-import {DataService} from "../shared/data.service";
-import {PaginationInstance} from "ng2-pagination";
-import {Router} from "@angular/router";
+import {ScoreCard, ScorecardService} from '../shared/services/scorecard.service';
+import {OrgUnitService} from '../shared/services/org-unit.service';
+import {DataService} from '../shared/data.service';
+import {PaginationInstance} from 'ng2-pagination';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +13,8 @@ import {Router} from "@angular/router";
   styleUrls: ['./home.component.css'],
   animations: [
     trigger('visibilityChanged', [
-      state('shown' , style({ opacity: 1 })),
-      state('hidden', style({ opacity: 0 })),
+      state('shown', style({opacity: 1})),
+      state('hidden', style({opacity: 0})),
       transition('shown => hidden', animate('600ms')),
       transition('hidden => shown', animate('300ms')),
     ])
@@ -26,100 +26,100 @@ export class HomeComponent implements OnInit {
   scorecards_loading: boolean;
   complete_percent: number;
   total: number = 0;
-  loading_message:string;
+  loading_message: string;
   queryterm: string = null;
   deleting: boolean[] = [];
   deleted: boolean[] = [];
   error_deleting: boolean[] = [];
   confirm_deleting: boolean[] = [];
-  have_authorities:boolean = true;
-  userInfo:any = {};
-  constructor( private scoreCardService: ScorecardService,
-               private orgUnitService: OrgUnitService,
-               private dataService: DataService,
-               private router: Router) {
+  have_authorities: boolean = true;
+  userInfo: any = {};
+  config: PaginationInstance = {
+    id: 'custom',
+    itemsPerPage: 3,
+    currentPage: 1
+  };
+  constructor(private scoreCardService: ScorecardService,
+              private orgUnitService: OrgUnitService,
+              private dataService: DataService,
+              private router: Router) {
     this.scorecards = [];
     this.scorecards_loading = true;
     this.complete_percent = 0;
-    this.loading_message = "Loading First Score card";
+    this.loading_message = 'Loading First Score card';
     this.dataService.getUserInformation().subscribe(
       userInfo => {
         this.userInfo = userInfo;
 
       }
-    )
+    );
   }
-
-  public config: PaginationInstance = {
-    id: 'custom',
-    itemsPerPage: 3,
-    currentPage: 1
-  };
-
   ngOnInit() {
     this.scoreCardService.loadAll().subscribe(
       scorecards => {
         let scorecard_count = 0;
-        for( let scorecard of scorecards ){
+        for (const scorecard of scorecards) {
           // loading scorecard details
           this.scoreCardService.load(scorecard).subscribe(
             scorecard_details => {
-              this.loading_message = "Loading data for "+scorecard_details.header.title;
-              let scorecard_item = {
+              this.loading_message = 'Loading data for ' + scorecard_details.header.title;
+              const scorecard_item = {
                 id: scorecard,
                 name: scorecard_details.header.title,
                 data: scorecard_details,
-                can_see:this.dataService.checkForUserGroupInScorecard(scorecard_details,this.userInfo).see,
-                can_edit:this.dataService.checkForUserGroupInScorecard(scorecard_details,this.userInfo).edit,
+                can_see: this.dataService.checkForUserGroupInScorecard(scorecard_details, this.userInfo).see,
+                can_edit: this.dataService.checkForUserGroupInScorecard(scorecard_details, this.userInfo).edit,
               };
-              this.scorecards.push(scorecard_item);
-              this.dataService.sortArrOfObjectsByParam(this.scorecards, 'name',true);
+              if (scorecard_item.can_see) {
+                this.scorecards.push(scorecard_item);
+              }
+              this.dataService.sortArrOfObjectsByParam(this.scorecards, 'name', true);
               this.deleting[scorecard] = false;
               this.confirm_deleting[scorecard] = false;
               this.deleted[scorecard] = false;
               this.error_deleting[scorecard] = false;
-              scorecard_count ++;
+              scorecard_count++;
               // set loading equal to false when all scorecards are loaded
-              if(scorecard_count == this.scorecards.length){
-                this.loading_message = "Done loading all score cards";
+              if (scorecard_count === this.scorecards.length) {
+                this.loading_message = 'Done loading all score cards';
                 this.scorecards_loading = false;
               }
-              this.complete_percent = (scorecard_count/this.scorecards.length)*100
+              this.complete_percent = (scorecard_count / this.scorecards.length) * 100
             },
             // catch error if anything happens when loading scorecard details
             detail_error => {
-              this.loading_message = "Error Occurred while loading scorecards";
+              this.loading_message = 'Error Occurred while loading scorecards';
               this.scorecards_loading = false;
             }
-          )
+          );
         }
       },
       // catch error when there is no scorecard
       error => {
         console.log('SOME ERROR OCCURRED');
-        this.loading_message = "Error Occurred while loading scorecards";
+        this.loading_message = 'Error Occurred while loading scorecards';
         this.scorecards_loading = false;
       }
     );
     this.orgUnitService.prepareOrgunits();
   }
 
-  openscorecard(id,event){
-    this.router.navigate(['create','edit',id]);
+  openscorecard(id, event) {
+    this.router.navigate(['create', 'edit', id]);
     event.stopPropagation();
   }
 
-  deleteScoreCard( scorecard, event ){
+  deleteScoreCard(scorecard, event) {
     event.stopPropagation();
     this.deleting[scorecard.id] = true;
     this.confirm_deleting[scorecard.id] = false;
-    this.scoreCardService.remove( scorecard ).subscribe(
+    this.scoreCardService.remove(scorecard).subscribe(
       data => {
         this.deleted[scorecard.id] = true;
         this.error_deleting[scorecard.id] = false;
         this.scorecards.forEach((item, index) => {
-          if( item.id == scorecard.id ){
-            this.scorecards.splice(index,1);
+          if (item.id == scorecard.id) {
+            this.scorecards.splice(index, 1);
           }
         });
       },
@@ -127,7 +127,7 @@ export class HomeComponent implements OnInit {
         this.deleted[scorecard.id] = false;
         this.deleting[scorecard.id] = false;
         this.error_deleting[scorecard.id] = true;
-        setTimeout(function() {
+        setTimeout(function () {
           this.error_deleting[scorecard.id] = false;
         }, 4000);
       }
